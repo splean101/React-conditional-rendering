3749751f66abdbafadc994118966a25fd282fcbb
splean101 <101splean@gmail.com>
2019-07-24 08:42:11 +0300

    fin

--- src/index.js
+++ src/index.js
@@ -4,33 +4,16 @@ import ReactDOM from 'react-dom';
 class CalcForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { value1: '', value1isValid: false, value2: '', value2isValid: false, res: 0 };
+        this.state = { value1: '', value2: '', res: 0 };
         this.onChange = this.onChange.bind(this);
         this.add = this.add.bind(this);
         this.sub = this.sub.bind(this);
         this.mul = this.mul.bind(this);
         this.div = this.div.bind(this);
     }
-    value1isValid(value1) {
-        if (!isNaN(value1)) {
-            return true;
-        }
-        return false;
-    }
-    value2isValid(value2) {
-        if (!isNaN(value2)) {
-            return true;
-        }
-        return false;
-    }
     onChange(event) {
         const { name, value } = event.target;
-        this.setState({ [name]: Number(value) });
-        if (name === 'value1') {
-            this.setState({ value1isValid: this.value1isValid(Number(value)) });
-        } else {
-            this.setState({ value2isValid: this.value2isValid(Number(value)) });
-        }
+        isNaN(value) ? alert([name] + ' is not a number') : this.setState({ [name]: Number(value) });
     }
     add() {
         return this.setState({ res: this.state.value1 + this.state.value2 });
@@ -42,18 +25,21 @@ class CalcForm extends React.Component {
         return this.setState({ res: this.state.value1 * this.state.value2 });
     }
     div() {
-        return this.setState({ res: this.state.value1 / this.state.value2 });
+        if (this.state.value2 !== 0) {
+            this.setState({ res: this.state.value1 / this.state.value2 })
+        } else {
+            this.setState({ res: 'Error! Division by zero is not allowed!' });
+            //alert('division by zero is not allowed');
+        }
     }
     render() {
-        let val1Color = this.state.value1isValid ? 'blue' : 'red';
-        let val2Color = this.state.value2isValid ? 'blue' : 'red';
         return (
             <form>
                 <label htmlFor="value1">value1:</label>
-                <input type="number" name="value1" value={this.state.value1} onChange={this.onChange} style={{ backgroundColor: val1Color }} />
+                <input name="value1" value={this.state.value1} onChange={this.onChange} />
                 <br />
                 <label htmlFor="value2">value2:</label>
-                <input type="number" name="value2" value={this.state.value2} onChange={this.onChange} style={{ backgroundColor: val2Color }} />
+                <input name="value2" value={this.state.value2} onChange={this.onChange} />
                 <br />
                 <button type="button" onClick={this.add}>+</button>
                 <button type="button" onClick={this.sub}>-</button>
